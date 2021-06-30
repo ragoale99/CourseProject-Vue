824b6e9f8c8f65067411f637743002d6c54bc59e
Raghesh99 <raghesh99@gmail.com>
2021-06-30 11:09:16 +0200

    delete resource

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -47,7 +47,8 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
-      addResource: this.addResource
+      addResource: this.addResource,
+      deleteResource: this.deleteResource
     };
   },
   computed: {
@@ -72,6 +73,10 @@ export default {
 
       this.storedResources.unshift(resource);
       this.selectedTab = 'stored-resources';
+    },
+    deleteResource(resId) {
+      const resIndex = this.storedResources.findIndex(res => res.id == resId);
+      this.storedResources.splice(resIndex, 1);
     }
   }
 };
